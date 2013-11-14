ecd4994488f29c2e63fc336e6db9fae231bf2ef5
tpltnt <tpltnt.github@dropcut.net>
2013-11-14 09:33:55 +0100

    docstring added

--- orgmode2json.py
+++ orgmode2json.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+A pure python 3 class to parse `orgmode <http://orgmode.org/>`_ files and
+spit out `JSON <http://wwww.json.org>`_
+
+.. moduleauthor:: tpltnt
+"""
 class Orgmode2json:
 
     def __init__():
