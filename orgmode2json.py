44b6f68def41f5b3e6ac919f8ee964a4f79f1bbd
tpltnt <tpltnt.github@dropcut.net>
2013-11-14 09:36:11 +0100

    code + doc fix

--- orgmode2json.py
+++ orgmode2json.py
@@ -2,11 +2,18 @@
 
 """
 A pure python 3 class to parse `orgmode <http://orgmode.org/>`_ files and
-spit out `JSON <http://wwww.json.org>`_
+spit out `JSON <http://wwww.json.org>`_. To this point it is mostly intended
+to parse (tagged) notes.
 
 .. moduleauthor:: tpltnt
 """
-class Orgmode2json:
+class Orgmode2json(self):
+    """
+    A pure python 3 class to parse `orgmode <http://orgmode.org/>`_ files and
+    spit out `JSON <http://wwww.json.org>`_.
 
-    def __init__():
+    .. moduleauthor:: tpltnt
+    """
+
+    def __init__(self):
         pass
