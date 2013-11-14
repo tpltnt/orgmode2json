9ac497cc1d3c8b3250d16582e402066122deac45
tpltnt <tpltnt.github@dropcut.net>
2013-11-14 10:16:33 +0100

    validity test added

--- tests/test_open_orgmodefile.py
+++ tests/test_open_orgmodefile.py
@@ -4,6 +4,14 @@ import pytest
 from orgmode2json import *
 
 
+def test_filename0():
+    """
+    Test to handle a valid filename. This should cause any trouble.
+    """
+    o2j = Orgmode2json()
+    o2j.open_orgmodefile('test.org')
+
+
 def test_filename1():
     """
     Test to handle non-string (int) as filename. This should fail with a
