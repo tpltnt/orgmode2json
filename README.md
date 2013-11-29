0dbf4a3d18729eb5b2e140b316d89599acd50d6a
tpltnt <tpltnt.github@dropcut.net>
2013-11-29 14:14:24 +0100

    initial (re)commit

--- README.md
+++ README.md
@@ -5,24 +5,3 @@ A simple [orgmode](http://orgmode.org/) to [JSON](http://json.org/)
 converter. It is most useful to work with tagged notes, but not any other
 stuff (so far).
 
-FAQ
----
-* What is the license?
-  All code is licensed under the [GNU Affero General Public License v3](https://www.gnu.org/licenses/agpl-3.0.en.html) unless explicitly stated otherwise.
-  I am willing to talk about other options.
-
-* Isn't the design rather byzantine?
-  Yes and no. A way shorter script might suffice. While this is correct with
-  respect to short-term payoffs the enforced modularized design facilitates
-  greater testing in detail and reuse in the future.
-
-* Were is the documentation?
-  It is in the source-code. You can extract it using [sphinx](http://sphinx-doc.org/) if you want to.
-
-* Are there tests supplied?
-  Yes, of course. Just run `py.test` to see what causes trouble.
-  Run `coverage report -m` to satisfy your OCD tendencies
-
-* Why didn't you include feature X? 
-  I most likely have other things to do. You can fork the code and send me a
-  pull request if you are desperate.
--- orgmode2json.py
+++ orgmode2json.py
@@ -1,78 +1,20 @@
 #!/usr/bin/env python3
 
 """
-A pure python 3 class to parse `orgmode <http://orgmode.org/>`_ files and
+A pure python 3 script to parse `orgmode <http://orgmode.org/>`_ files and
 spit out `JSON <http://wwww.json.org>`_. To this point it is mostly intended
 to parse (tagged) notes. This work is licensed under the `GNU Affero General Public License v3 <https://www.gnu.org/licenses/agpl-3.0.en.html>`_.
 
 .. moduleauthor:: tpltnt
 """
-class Orgmode2json(object):
-    """
-    A pure python 3 class to parse `orgmode <http://orgmode.org/>`_ files and
-    spit out `JSON <http://wwww.json.org>`_.
+from io import TextIOWrapper
+import sys
 
-    .. moduleauthor:: tpltnt
-    """
-    from io import TextIOWrapper
+orgmode_inputfile = open(sys.argv[1], 'r')
+json_outputfile = open(sys.argv[2], 'w')
 
-    __orgmode_inputfile = None
-    __json_outputfile = None
+if not isinstance(orgmodefile, TextIOWrapper):
+    raise TypeError("orgmode file of wrong type")
+if not isinstance(jsonfile, TextIOWrapper):
+    raise TypeError("JSON file of wrong type")
 
-    def __init__(self):
-        pass
-
-    def open_orgmodefile(self, filename):
-        """
-        Open a orgmode file for reading. The file is identified by the given
-        name.
-
-        :param filename: name (path) of the file to open
-        :type filename: str
-        :returns: file object associated with the file
-        :raises: TypeError
-        """
-
-        if not isinstance(filename,str):
-            raise TypeError("given filename is not a string")
-
-        __ofile = open(filename,'r')
-        return __ofile
-
-
-    def open_jsonfile(self, filename):
-        """
-        Open a JSON file for reading. The file is identified by the given
-        name.
-
-        :param filename: name (path) of the file to open
-        :type filename: str
-        :returns: file object associated with the file
-        :raises: TypeError
-        """
-
-        if not isinstance(filename,str):
-            raise TypeError("given filename is not a string")
-
-        __ofile = open(filename,'w')
-        return __ofile
-
-
-    def parse_and_write(self, orgmodefile, jsonfile):
-        """
-        Parse the given orgmode-file and write into the given JSON file.
-
-        :param orgmodefile:
-        :type orgmodefile: io.TextIOWrapper
-        :param jsonfile:
-        :type jsonfile: io.TextIOWrapper
-        :raises: TypeError
-        """
-        from io import TextIOWrapper
-
-        if not isinstance(orgmodefile, TextIOWrapper):
-            raise TypeError("orgmode file of wrong type")
-        if not isinstance(jsonfile, TextIOWrapper):
-            raise TypeError("JSON file of wrong type")
-
-        pass
